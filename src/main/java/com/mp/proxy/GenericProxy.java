ac2f760feccae7c3192584f608f8186991f59b71
wudunhong <wudunhong@12301.com>
2023-03-20 18:13:41 +0800

    整合feign

--- src/main/java/com/mp/proxy/GenericProxy.java
+++ src/main/java/com/mp/proxy/GenericProxy.java
@@ -1,8 +1,9 @@
 package com.mp.proxy;
 
+import com.mp.proxy.config.GenericConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "generic", url = "https://www.baidu.com")
+@FeignClient(name = "generic", url = "https://www.baidu.com", configuration = GenericConfig.class)
 public interface GenericProxy {
 
 }
