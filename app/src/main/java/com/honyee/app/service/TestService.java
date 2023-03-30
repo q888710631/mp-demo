dfab4026390eed5d767e8bb20d3ab18d0c51190d
wudunhong <wudunhong@12301.com>
2023-03-30 16:22:12 +0800

    修改readme

--- app/src/main/java/com/honyee/app/service/TestService.java
+++ app/src/main/java/com/honyee/app/service/TestService.java
@@ -1,13 +1,8 @@
 package com.honyee.app.service;
 
-import com.honyee.app.config.delay.DelayTaskConfiguration;
 import com.honyee.app.config.lock.RedisLock;
-import com.honyee.app.delay.MyDelayParam;
 import com.honyee.app.utils.LogUtil;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class TestService {
