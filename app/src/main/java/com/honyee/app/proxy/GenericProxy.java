4be6e7d52ff0e9a4975652d974c9b4b4043b1b5c
wudunhong <wudunhong@12301.com>
2023-05-06 09:07:41 +0800

    fix:GenericProxy

--- app/src/main/java/com/honyee/app/proxy/GenericProxy.java
+++ app/src/main/java/com/honyee/app/proxy/GenericProxy.java
@@ -3,7 +3,7 @@ package com.honyee.app.proxy;
 import com.honyee.app.proxy.config.GenericConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "genericcom.honyee.proxy", url = "https://www.baidu.com", configuration = GenericConfig.class)
+@FeignClient(name = "generic", url = "https://www.baidu.com", configuration = GenericConfig.class)
 public interface GenericProxy {
 
 }
