89b56a9dc57b0b8c07a9511887ec117cd1e4e3ec
honyee <honyee@qq.com>
2023-05-07 20:34:23 +0800

    DataBindUtil.toCamelCase优化

--- app/src/main/java/com/honyee/app/utils/DataBindUtil.java
+++ app/src/main/java/com/honyee/app/utils/DataBindUtil.java
@@ -4,9 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.CaseFormat;
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.validation.DataBinder;
-
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
@@ -18,19 +15,6 @@ public class DataBindUtil {
     }
 
     public static <T> T bind(Class<T> clz, Map<String, Object> data) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-         /*
-            待解决：
-            复杂嵌套对象转换失败：org.springframework.beans.convertIfNecessary
-            conversionnotsupportedexception： 无法将LinkedHashMap转成具体对象
-         */
-//        T t = clz.getConstructor().newInstance();
-//        DataBinder dataBinder = new DataBinder(t);
-//        // Set ignored*
-//        dataBinder.setIgnoreInvalidFields(true);
-//        dataBinder.setIgnoreUnknownFields(true);
-//        MutablePropertyValues propertyValues = new MutablePropertyValues(data);
-//        dataBinder.bind(propertyValues);
-//        return t;
         try {
             return OBJECT_MAPPER.readValue(OBJECT_MAPPER.writeValueAsString(data), clz);
         } catch (JsonProcessingException e) {
@@ -48,11 +32,53 @@ public class DataBindUtil {
         List<String> keyStrings = new ArrayList<>();
         while (tokenizer.hasMoreTokens()) {
             String newKey = tokenizer.nextToken();
-            if (newKey.contains("-")) {
-                newKey = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, newKey);
-            }
+            // 不支持横杠和下划线混合处理，通过replace替换一遍
+            newKey = newKey.replace("-","_");
+            newKey = CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, newKey);
             keyStrings.add(newKey);
         }
         return String.join(".", keyStrings);
+    }
+
+    /**
+     * 处理Map中的key
+     *
+     * @param map 例如key为 hello-world
+     * @return 转换成 helloWorld
+     */
+    public static Map<String, Object> dealMapKey(Map<?, ?> map) {
+        Set<?> set = map.keySet();
+        Iterator<?> iterator = set.iterator();
+        Map<String, Object> result = new LinkedHashMap<>();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            if (next instanceof String) {
+                Object value = map.get(next);
+                if (value instanceof Map) {
+                    value = dealMapKey((Map<?, ?>) value);
+                } else if (value instanceof Collection) {
+                    value = dealCollection(value);
+                }
+                String key = DataBindUtil.toCamelCase(next.toString());
+                result.put(key, value);
+            } else {
+                throw new UnsupportedOperationException("不支持key不为String的情况");
+            }
+        }
+        return result;
+    }
+
+    public static Object dealCollection(Object value) {
+        if (value instanceof Map) {
+            return dealMapKey((Map<?,?>) value);
+        } else if (value instanceof Collection) {
+            Collection<?> collection = (Collection<?>) value;
+            List<Object> collectionResult = new ArrayList<>();
+            for (Object child : collection) {
+                collectionResult.add(dealCollection(child));
+            }
+            return collectionResult;
+        }
+        return value;
     }
 }
--- app/src/main/java/com/honyee/app/utils/YamlUtil.java
+++ app/src/main/java/com/honyee/app/utils/YamlUtil.java
@@ -10,50 +10,15 @@ public class YamlUtil {
     public static <T> T loadAs(InputStream input, Class<T> clz) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Yaml yaml = new Yaml();
         Map map = yaml.loadAs(input, Map.class);
-        Map<String, Object> result = dealMapKey(map);
+        Map<String, Object> result = DataBindUtil.dealMapKey(map);
         return DataBindUtil.bind(clz, result);
     }
 
     public static <T> T loadAs(String config, Class<T> clz) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         Yaml yaml = new Yaml();
         Map map = yaml.loadAs(config, Map.class);
-        Map<String, Object> result = dealMapKey(map);
+        Map<String, Object> result = DataBindUtil.dealMapKey(map);
         return DataBindUtil.bind(clz, result);
     }
 
-    private static Map<String, Object> dealMapKey(Map map) {
-        Set set = map.keySet();
-        Iterator iterator = set.iterator();
-        Map<String, Object> result = new LinkedHashMap<>();
-        while (iterator.hasNext()) {
-            Object next = iterator.next();
-            if (next instanceof String) {
-                Object value = map.get(next);
-                if (value instanceof Map) {
-                    value = dealMapKey((Map) value);
-                } else if (value instanceof Collection) {
-                    value = dealCollection(value);
-                }
-                String key = DataBindUtil.toCamelCase(next.toString());
-                result.put(key, value);
-            } else {
-                throw new UnsupportedOperationException("不支持key不为String的情况");
-            }
-        }
-        return result;
-    }
-
-    private static Object dealCollection(Object value) {
-        if (value instanceof Map) {
-            return dealMapKey((Map) value);
-        } else if (value instanceof Collection) {
-            Collection collection = (Collection) value;
-            List collectionResult = new ArrayList();
-            for (Object child : collection) {
-                collectionResult.add(dealCollection(child));
-            }
-            return collectionResult;
-        }
-        return value;
-    }
 }
