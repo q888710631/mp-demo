a0006edca1542a1f853d8604c57de16a6b8292a0
wudunhong <wudunhong@12301.com>
2023-05-11 17:07:38 +0800

    整合easy-excel

--- app/src/main/java/com/honyee/app/utils/ExcelUtil.java
+++ app/src/main/java/com/honyee/app/utils/ExcelUtil.java
@@ -1,8 +1,13 @@
 package com.honyee.app.utils;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.write.metadata.WriteSheet;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class ExcelUtil {
     public static final String HEADER_CONTENT_TYPE_DOWNLOAD_EXCEL = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
@@ -17,6 +22,38 @@ public class ExcelUtil {
         response.setContentType(HEADER_CONTENT_TYPE_DOWNLOAD_EXCEL);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setHeader(HEADER_CONTENT_DISPOSITION_KEY, String.format(HEADER_CONTENT_DISPOSITION_VALUE, encodeFileName));
+    }
+
+    /**
+     * 构建工作簿
+     * @param title 工作簿名称
+     * @param clz 输出数据的DTO
+     */
+    public static WriteSheet buildWriteSheet(String title, Class<?> clz) {
+        return EasyExcel.writerSheet(title).head(clz).build();
+    }
+    /**
+     * 输出数据
+     */
+    public static void write(HttpServletResponse response, String fileName, List<?> list, Class<?> clz) throws IOException {
+        write(response, fileName, list, buildWriteSheet("Sheet1", clz));
+    }
+
+    /**
+     * 输出数据
+     */
+    public static void write(HttpServletResponse response, String fileName, List<?> list, WriteSheet sheet) throws IOException {
+        ExcelUtil.initResponse(response, fileName);
+        ExcelWriter writer = null;
+        try {
+
+            writer = EasyExcel.write(response.getOutputStream()).build();
+            writer.write(list, sheet);
+        } finally {
+            if (writer != null) {
+                writer.finish();
+            }
+        }
     }
 
 }
